018016d5caa8f17169cb95dfc85d9124fea2e926
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2021-12-12 11:42:14 -0800

    remove frontend

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <p className="footer__text">&copy; 2021 Around the U.S.</p>
-    </footer>
-  );
-};
-
-export default Footer;
