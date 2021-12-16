4e29dac28610944d448a9f0770f2d7f9160b4418
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2021-12-15 17:02:43 -0800

    edit

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(requestLogger); // enabling the request logger
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 1000, // limit each IP to 100 requests per windowMs
 });
 
 app.use(limiter);
