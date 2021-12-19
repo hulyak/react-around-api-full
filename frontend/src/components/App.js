db4a281310c1099822703fbc4e942141a601fc7c
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2021-12-18 16:24:00 -0800

    edit

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -199,10 +199,10 @@ function App() {
 
   const handleCardLike = (card) => {
     // Check one more time if this card was already liked
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((i) => i === currentUser._id);
     // Send a request to the API and getting the updated card data
     api
-      .changeLikeCardStatus(card._id, !isLiked)
+      .changeLikeCardStatus(card._id, !isLiked, jwt)
       .then((newCard) => {
         setCards((state) =>
           state.map((c) => (c._id === card._id ? newCard : c))
