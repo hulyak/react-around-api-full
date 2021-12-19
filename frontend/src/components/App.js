b2778219f5e06d03160227ecee777557bdb0a457
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2021-12-18 16:39:53 -0800

    edit

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -181,28 +181,15 @@ function App() {
   }, [jwt, api, CurrentUserContext]);
 
   // API CALLS
-  // const handleCardLike = (card) => {
-  //   const isLiked =
-  //     card.likes !== undefined ? card.likes.includes(currentUser._id) : false;
-  //   const handleLike = !isLiked
-  //     ? api.addLike(card._id, jwt)
-  //     : api.deleteCard(card._id, jwt);
-
-  //   handleLike
-  //     .then((newCard) => {
-  //       setCards((state) =>
-  //         state.map((c) => (c._id === card._id ? newCard : c))
-  //       );
-  //     })
-  //     .catch((e) => console.error(e));
-  // };
-
   const handleCardLike = (card) => {
     // Check one more time if this card was already liked
     const isLiked = card.likes.some((i) => i._id === currentUser._id);
     // Send a request to the API and getting the updated card data
-    api
-      .changeLikeCardStatus(card._id, !isLiked)
+    const handleLike = !isLiked
+      ? api.addLike(card._id, jwt)
+      : api.deleteLike(card._id, jwt);
+
+    handleLike
       .then((newCard) => {
         setCards((state) =>
           state.map((c) => (c._id === card._id ? newCard : c))
