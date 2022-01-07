d9204f8e9327b7c683bcd8870cd0e9f604c1ad8b
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2022-01-06 20:39:01 -0800

    edit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,34 +1,37 @@
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
+import { FaAlignRight } from "react-icons/fa";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../images/Logo.svg";
 
 const Header = ({ title, userEmail, onLogout, link }) => {
-  // const [toggleMenu, setToggleMenu] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  // const handleToggleButton = () => {
-  //   setToggleMenu(!toggleMenu);
-  // };
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <header className="header">
-      <img src={logo} alt="Logo of Around the U.S" className="header__logo" />
-      {/* <FontAwesomeIcon icon={faBars} size="lg" onClick={handleToggleButton} /> */}
-      {/* {toggleMenu && ( */}
-      <ul className="header__nav">
-        {userEmail && (
+      <div className="header__center">
+        <img src={logo} alt="Logo of Around the U.S" className="header__logo" />
+
+        <button type="button" className="header__button" onClick={handleToggle}>
+          <FaAlignRight className="nav-icon" />
+        </button>
+
+        <ul className={isOpen ? "header__nav show-nav" : "header__nav"}>
+          {userEmail && (
+            <li>
+              <p className="header__email">{userEmail}</p>
+            </li>
+          )}
           <li>
-            <p className="header__email">{userEmail}</p>
+            <Link to={link} onClick={onLogout} className="header__logout">
+              {title}
+            </Link>
           </li>
-        )}
-        <li>
-          <Link to={link} onClick={onLogout} className="header__logout">
-            {title}
-          </Link>
-        </li>
-      </ul>
-      {/* )} */}
+        </ul>
+      </div>
     </header>
   );
 };
